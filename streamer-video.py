b2ed746aa41a88340626480f566699ed4aba407e
Jonathan Lehner <lehnerj@student.ethz.ch>
2019-10-18 18:13:41 +0200

    solved

---
+++ streamer-video.py
@@ -0,0 +1,15 @@
+# run this program on each RPi to send a labelled image stream
+import socket
+import time
+from imutils.video import FileVideoStream
+import imagezmq
+ 
+sender = imagezmq.ImageSender(connect_to='tcp://10.1.1.60:5555')
+ 
+rpi_name = socket.gethostname() # send RPi hostname with each image
+picam = FileVideoStream("/Users/Jonathanlehner/Asianriding.mp4").start()
+time.sleep(2.0)  # allow camera sensor to warm up
+
+while True:  # send images as stream until Ctrl-C
+       image = picam.read()
+       sender.send_image(rpi_name, image)
